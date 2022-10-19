a62f4db78901920afdc588fef5cc56a8f5c9ec1e
handlename <nagata@handlena.me>
2022-10-19 15:45:16 +0900

    ssmwrap v1.2.1

--- ssmwrap.rb
+++ ssmwrap.rb
@@ -1,13 +1,13 @@
 class Ssmwrap < Formula
-  version '1.2.0'
+  version '1.2.1'
   homepage 'https://github.com/handlename/ssmwrap'
   if OS.mac?
-    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.0/ssmwrap_v1.2.0_darwin_amd64.zip"
-    sha256 '265759c96e9c18121d7c6658754a9dd7e6e81807667e9fd0aa82542519c26b12'
+    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.1/ssmwrap_v1.2.1_darwin_amd64.zip"
+    sha256 '2644d40f21d9ca62b49ec128a6e6f242d3183fe93bcafb5e440c2d72d38a363c'
   end
   if OS.linux?
-    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.0/ssmwrap_v1.2.0_linux_amd64.tar.gz"
-    sha256 'da014425d03028674de73af02f64ee4b50865645267d178b108e2b93d4e4089a'
+    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.1/ssmwrap_v1.2.1_linux_amd64.tar.gz"
+    sha256 '65f38e8ba154e438eee960f8fabbfbaccdb7d82d42d8f7015f6b4eb7c7f11b39'
   end
   head 'https://github.com/handlename/ssmwrap.git'
 
