d6ed526814086f4f555c0e726cfcebcffa837f61
handlename <nagata@handlena.me>
2024-05-20 13:10:31 +0900

    ssmwrap v2.1.0

--- ssmwrap.rb
+++ ssmwrap.rb
@@ -1,17 +1,32 @@
 class Ssmwrap < Formula
-  version '1.2.1'
+  desc 'Exec command with environment variables loaded from AWS SSM.'
+  version '2.1.0'
   homepage 'https://github.com/handlename/ssmwrap'
-  if OS.mac?
-    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.1/ssmwrap_v1.2.1_darwin_amd64.zip"
-    sha256 '2644d40f21d9ca62b49ec128a6e6f242d3183fe93bcafb5e440c2d72d38a363c'
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_darwin_arm64.tar.gz'
+      sha256 '7f857eb6c91d23905ebf1affe4dfa7cb93c83bf4c5c36b39c29b4ffe77bf7950'
+    end
+    if Hardware::CPU.intel?
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_darwin_amd64.tar.gz'
+      sha256 '676d57191b314ca503e7ecefddcd635e500c158dec2c810f240b81eacb2decc4'
+    end
   end
-  if OS.linux?
-    url "https://github.com/handlename/ssmwrap/releases/download/v1.2.1/ssmwrap_v1.2.1_linux_amd64.tar.gz"
-    sha256 '65f38e8ba154e438eee960f8fabbfbaccdb7d82d42d8f7015f6b4eb7c7f11b39'
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_linux_arm64.tar.gz'
+      sha256 'e1379d1eda76e7bcc007fe751a0ea012abc0355cb30445024cb227d013da15cf'
+    end
+    if Hardware::CPU.intel?
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_linux_amd64.tar.gz'
+      sha256 '3d7c61f40a383f96b9d8ac2a7b41711ebff2923634248a356a8a4d09089d41b0'
+    end
   end
-  head 'https://github.com/handlename/ssmwrap.git'
 
   head do
+    url 'https://github.com/handlename/ssmwrap.git'
     depends_on 'go' => :build
   end
 
