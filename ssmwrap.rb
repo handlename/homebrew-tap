2240b3bb32b817d47fa005f5d1c351ad07ab3739
handlename <nagata@handlena.me>
2024-07-02 09:02:14 +0900

    feat: ssmwrap v2.2.0

--- ssmwrap.rb
+++ ssmwrap.rb
@@ -1,27 +1,27 @@
 class Ssmwrap < Formula
-  desc 'Exec command with environment variables loaded from AWS SSM.'
-  version '2.1.0'
+  desc 'Execute command with environment variables or/and files loaded from AWS SSM.'
+  version '2.2.0'
   homepage 'https://github.com/handlename/ssmwrap'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_darwin_arm64.tar.gz'
-      sha256 '7f857eb6c91d23905ebf1affe4dfa7cb93c83bf4c5c36b39c29b4ffe77bf7950'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.0/ssmwrap_2.2.0_darwin_arm64.tar.gz'
+      sha256 'ff46c74a773dca234217abab237e8945dbbdad41418b520fec0dcbed25ac79d3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_darwin_amd64.tar.gz'
-      sha256 '676d57191b314ca503e7ecefddcd635e500c158dec2c810f240b81eacb2decc4'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.0/ssmwrap_2.2.0_darwin_amd64.tar.gz'
+      sha256 '3bc21e33abd2e6ea4c631de2f3520fccb4813cc2a2a223b5c030f17ace579193'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_linux_arm64.tar.gz'
-      sha256 'e1379d1eda76e7bcc007fe751a0ea012abc0355cb30445024cb227d013da15cf'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.0/ssmwrap_2.2.0_linux_arm64.tar.gz'
+      sha256 'a9717422da51ce262c9ddd6543c480f76f58cfb99e66a2d30377516f36e79dfd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.1.0/ssmwrap_2.1.0_linux_amd64.tar.gz'
-      sha256 '3d7c61f40a383f96b9d8ac2a7b41711ebff2923634248a356a8a4d09089d41b0'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.0/ssmwrap_2.2.0_linux_amd64.tar.gz'
+      sha256 'bce235287302c18a0c58a38fde7b793d2a183cccfe87ff35c7a55eefc79a5386'
     end
   end
 
