9f00095ffa209cfc068dd288d3e5ab764aa69a43
handlename <nagata@handlena.me>
2024-07-02 09:02:03 +0900

    feat: renew mdqi.rb

--- mdqi.rb
+++ mdqi.rb
@@ -1,11 +1,24 @@
 class Mdqi < Formula
+  desc 'command line interface for mdq like mysql cli.'
   version '0.0.3'
   homepage 'https://github.com/handlename/mdqi'
-  url "https://github.com/handlename/mdqi/releases/download/v0.0.3/mdqi-v0.0.3-darwin-amd64"
-  sha256 'd1c36ea573eb65add734ed5927effb32af8d5f8cb21b27d38c58aeead86ca8a1'
-  head 'https://github.com/handlename/mdqi.git'
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url 'https://github.com/handlename/mdqi/releases/download/v0.0.3/mdqi-v0.0.3-darwin-amd64'
+      sha256 'd1c36ea573eb65add734ed5927effb32af8d5f8cb21b27d38c58aeead86ca8a1'
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url 'https://github.com/handlename/mdqi/releases/download/v0.0.3/mdqi-v0.0.3-linux-amd64'
+      sha256 '290cb36857d0e0e2d3bd91494f276340151839ddbe05244428ba96ef7e44c827'
+    end
+  end
 
   head do
+    url 'https://github.com/handlename/mdqi.git'
     depends_on 'go' => :build
   end
 
