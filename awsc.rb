e936a86cb47a3e6d2b925cf7b4cd313d7a01b803
NAGATA Hiroaki <nagata@handlena.me>
2024-12-23 17:40:30 +0900

    feat: awsc v0.2.1

--- awsc.rb
+++ awsc.rb
@@ -1,26 +1,26 @@
 class Awsc < Formula
-  version '0.2.0'
+  version '0.2.1'
   homepage 'https://github.com/handlename/awsc'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/handlename/awsc/releases/download/v0.2.0/awsc_0.2.0_darwin_arm64.tar.gz'
-      sha256 '3f7427c143991670486a832676b2bdd7d42eaf4984711e47065396300f88883e'
+      url 'https://github.com/handlename/awsc/releases/download/v0.2.1/awsc_0.2.1_darwin_arm64.tar.gz'
+      sha256 '19082b41f89dbb6d06e534ade235ce9c85ee5f2b1ac46409bde16c2f4c0313e6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/awsc/releases/download/v0.2.0/awsc_0.2.0_darwin_amd64.tar.gz'
-      sha256 '5fd00ec40b3c9f36b082d96bfb7b1fd0c40a29d2b19b80da6ad88e415d806571'
+      url 'https://github.com/handlename/awsc/releases/download/v0.2.1/awsc_0.2.1_darwin_amd64.tar.gz'
+      sha256 '238be7a24341ab8a370b4c5c9b5f129fbb45a001a5d3d2f709f1b6496e021b1c'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/handlename/awsc/releases/download/v0.2.0/awsc_0.2.0_linux_arm64.tar.gz'
-      sha256 'c57c494b88923c9f455b23fb8e937be18fef29c19ac3857666ad63376a2875f4'
+      url 'https://github.com/handlename/awsc/releases/download/v0.2.1/awsc_0.2.1_linux_arm64.tar.gz'
+      sha256 '945a11b84eaccdf13ca0da5a6eb51e3154e5773d0698401dc63a75023451b182'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/awsc/releases/download/v0.2.0/awsc_0.2.0_linux_amd64.tar.gz'
-      sha256 '36fe9b9bcb524ec9f62851a9453e9d25ac5199eecc4d2c6877fbcf188f34c13b'
+      url 'https://github.com/handlename/awsc/releases/download/v0.2.1/awsc_0.2.1_linux_amd64.tar.gz'
+      sha256 '85ce1475164cf111cb1290002d22654c890b6a0c540656d08228e0ea514a4ee4'
     end
   end
 
