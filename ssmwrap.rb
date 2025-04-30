ca8115e3be8d2f89718a832408200d07b44cfd62
NAGATA Hiroaki <nagata@handlena.me>
2025-04-30 23:36:16 +0900

    feat: ssmwrap v2.2.3

--- ssmwrap.rb
+++ ssmwrap.rb
@@ -1,27 +1,27 @@
 class Ssmwrap < Formula
   desc 'Execute command with environment variables or/and files loaded from AWS SSM.'
-  version '2.2.2'
+  version '2.2.3'
   homepage 'https://github.com/handlename/ssmwrap'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.2/ssmwrap_2.2.2_darwin_arm64.tar.gz'
-      sha256 '3056b15a9d45bf9411b8a0fc2cef445a98978131dd0c7a248db8314bc2eea9ef'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.3/ssmwrap_2.2.3_darwin_arm64.tar.gz'
+      sha256 '7702121822dcffa5b5878a4691891e092b962c9182ac35e544d8e29e6c417591'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.2/ssmwrap_2.2.2_darwin_amd64.tar.gz'
-      sha256 '8f06fd1a949b7093684f846cca1669d1188a6993a466116f344cda6d5864b496'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.3/ssmwrap_2.2.3_darwin_amd64.tar.gz'
+      sha256 'd4c835887d12a93dfce92c95150d9e9db2fdf810f4aa180fc3c36700642fbed0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.2/ssmwrap_2.2.2_linux_arm64.tar.gz'
-      sha256 '3fc1aac9ad14ff2e4506673c7fc7a3e079c6d6d52997a02ada5eea1d834055f6'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.3/ssmwrap_2.2.3_linux_arm64.tar.gz'
+      sha256 'b84a668774696f22ef8f4aafabbf97afc309334a5b7847588633e4ae3c3ee28f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.2/ssmwrap_2.2.2_linux_amd64.tar.gz'
-      sha256 'ef8296a763828e8abb7ccb98644c9d2bb91590237d8ae350df86baa935a165ce'
+      url 'https://github.com/handlename/ssmwrap/releases/download/v2.2.3/ssmwrap_2.2.3_linux_amd64.tar.gz'
+      sha256 '3d0602498028285d116a0a835271c47de08403e65a0bc220367c42bf9b121bba'
     end
   end
 
